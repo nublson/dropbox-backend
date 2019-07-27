724819bfa94a68b8b998d048f10c947a52db5640
Nubelson <therealnletter@hotmail.com>
2019-07-28 00:41:56 +0100

    Database connection and project organization

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
+//! Start App
 const app = express();
+
+//! Some variables
 const port = process.env.PORT || 3001;
+
+//! Permissions
+//* Waiting...
+
+//! DataBase
+const databaseUrl = process.env.MONGO_URL;
+mongoose.connect(databaseUrl, { useNewUrlParser: true });
+
+//! Routes
 app.use(require('./routes'));
 
+//! Listen Ports
 app.listen(port, () => console.log(`Port ${port} is open.`));
