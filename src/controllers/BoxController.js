86cf1e6936f882651e393212e2180a2fabc76dab
Nubelson <therealnletter@hotmail.com>
2019-07-28 02:11:55 +0100

    Reading the box properties

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -5,6 +5,12 @@ class BoxController {
 		const box = await Box.create(req.body);
 		return res.json(box);
 	}
+
+	async show(req, res) {
+		const box = await Box.findById(req.params.id);
+
+		return res.json(box);
+	}
 }
 
 module.exports = new BoxController();
--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,11 @@ const upload = multer(multerConfig);
 const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
 
-//! routes
+//! Box routes
+routes.get('/box/:id', BoxController.show);
 routes.post('/box', BoxController.store);
+
+//! File routes
 routes.post('/box/:id/files', upload.single('file'), FileController.store);
 
 module.exports = routes;
