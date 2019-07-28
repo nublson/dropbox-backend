6ac95b311e805a05e240bd8f524dac5a3bf9b436
Nubelson <therealnletter@hotmail.com>
2019-07-28 05:53:29 +0100

    dotenv

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-require('dotenv/config');
+require('dotenv').config();
 
 //! Start App
 const app = express();
