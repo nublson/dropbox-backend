c0b0b91f8f86471d8745d18faf67819bf30dab6a
Nubelson <therealnletter@hotmail.com>
2019-07-28 02:16:09 +0100

    populate the relationship between box and file

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -7,7 +7,10 @@ class BoxController {
 	}
 
 	async show(req, res) {
-		const box = await Box.findById(req.params.id);
+		const box = await Box.findById(req.params.id).populate({
+			path: 'files',
+			options: { sort: { createdAt: -1 } }
+		});
 
 		return res.json(box);
 	}
