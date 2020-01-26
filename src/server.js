aa4dddd528d823061b69bbc32a3e188f6c9cf9b9
nubelsondev <nubelsondev@hotmail.com>
2020-01-26 07:22:44 +0000

    initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { connect } = require('mongoose')
+require('dotenv/config')
+
+const port = process.env.PORT || 3333
+const dbUrl = process.env.MONGO_URL
+const routes = require('./routes')
+
+const app = express()
+app.use(express.json())
+app.use(routes)
+
+connect(dbUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+app.listen(port, () => console.log(`Port ${port} is open...`))
