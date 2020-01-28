f1881c2b3e8ae8d3c37b9a0cdbd2b1373f4cbe01
nubelsondev <nubelsondev@hotmail.com>
2020-01-28 18:24:00 +0000

    add Show method

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -4,14 +4,22 @@ module.exports = {
 	async store(req, res) {
 		const { title } = req.body
 
-		let box = await Box.findOne({ title }).populate({
-			path: 'files',
-			options: { sort: { createdAt: -1 } }
-		})
+		let box = await Box.findOne({ title })
 
 		if (!box) {
 			box = await Box.create({ title })
 		}
+
+		return res.json(box)
+	},
+
+	async show(req, res) {
+		const { id } = req.params
+
+		const box = await Box.findById(id).populate({
+			path: 'files',
+			options: { sort: { createdAt: -1 } }
+		})
 
 		return res.json(box)
 	}
